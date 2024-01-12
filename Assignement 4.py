96a69c989fc3755b14c180277c64015ec7dfd515
Jazper26 <jazperhaapin89Gmail.com>
2024-01-12 16:11:40 +0800

    Ask user to input 3 numbers

---
+++ Assignement 4.py
@@ -0,0 +1,8 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+# Ask user to input 3 numbers.
+num1 = float (input ( "Enter the 1st number: "))
+num2 = float (input ( "Enter the 2nd number: "))
+num3 = float (input ( "Enter the 3rd number: "))
+
+# Find and print the biggest number using only if-else statement
