cbb9a456010490ae1bca3d78331d0c7bc42e66c0
Jazper26 <jazperhaapin89Gmail.com>
2024-01-12 16:18:35 +0800

    Compare to find the biggest number using if else statement

--- Assignement 4.py
+++ Assignement 4.py
@@ -6,3 +6,10 @@ num2 = float (input ( "Enter the 2nd number: "))
 num3 = float (input ( "Enter the 3rd number: "))
 
 # Find and print the biggest number using only if-else statement
+# Compare the numbers to find the biggest number
+if num1 >= num2 and num1 >= num3;
+    biggest = num1
+if num2 >= num1 and num2 >= num3;
+    biggest = num2
+if num3 >= num1 and num3 >= num2;
+    biggest = num3
